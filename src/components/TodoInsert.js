98b16ed339b86211acf036b485dc9ce48af6554a
ko <ko>
2020-09-02 20:34:51 +0900

    항목 추가 기능 구현

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(
+        (e) => {
+            setValue(e.target.value);
+            // console.log(e.target.value);
+        },
+        [],
+    )
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+
+            e.preventDefault();
+        },
+        [onInsert, value],
+        console.log([onInsert, value]),
+    );
+
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder="할 일을 입력하세요" 
+                onChange={onChange}
+                value={value}
+                />
             <button type="submit">
                 <MdAdd />
             </button>
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,17 +4,19 @@ import {
     MdCheckBox,
     MdRemoveCircleOutline,
 } from 'react-icons/md';
+import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
+const TodoListItem = ({todo}) => {
+    const {text, checked} = todo;
     return (
         <div className="TodoListItem">
-            <div className="checkbox">
-                <MdCheckBoxOutlineBlank />
-                <div className="text">할 일</div>
-            </div>
-            <div className="remove">
-                <MdRemoveCircleOutline />
+            <div className={cn('checkbox', { checked })}>
+            {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+            <div className="text">{text}</div>
+                <div className="remove">
+                    <MdRemoveCircleOutline />
+                </div>
             </div>
         </div>
     )
