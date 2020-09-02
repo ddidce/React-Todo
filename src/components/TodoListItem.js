354ea671876d1525e730463ce7e8b924a4d86e13
ko <ko>
2020-09-02 20:58:13 +0900

    수정기능

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,12 +7,12 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({todo, onRemove}) => {
+const TodoListItem = ({todo, onRemove, onToggle}) => {
     const {text, checked, id} = todo;
-    console.log(text, checked, id);
+    console.log(`내용 : ${text}, 체크여부 : ${checked}, id값 : ${id}`);
     return (
         <div className="TodoListItem">
-            <div className={cn('checkbox', { checked })}>
+            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
             {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
             <div className="text">{text}</div>
                 <div className="remove" onClick={() => onRemove(id)}>
